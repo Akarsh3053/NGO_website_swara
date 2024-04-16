707c48de9a47d6ed7c6f4e7598b2d61d7fa7ede3
Akarsh Bajpai <onlyakarsh@gmail.com>
2024-04-16 22:23:25 +0530

    add new config

--- components/Contact.tsx
+++ components/Contact.tsx
@@ -13,14 +13,53 @@ import {
 } from "@/components/ui/drawer"
 import { Input } from "./ui/input"
 import { Textarea } from "./ui/textarea"
+import { useState } from "react"
+import emailjs from "@emailjs/browser";
 
 
 export function Contact() {
-  const [goal, setGoal] = React.useState(350)
 
-  function onClick(adjustment: number) {
-    setGoal(Math.max(200, Math.min(400, goal + adjustment)))
-  }
+  const [formData, setFormData] = useState({
+    username: '',
+    email: '',
+    query: '',
+  });
+
+  //@ts-ignore
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData(prevState => ({
+      ...prevState,
+      [name]: value,
+    }));
+  };
+
+  //@ts-ignore
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    emailjs
+      .send(
+        'service_g4a1kig',
+        'template_6c1nees',
+        {
+          from_name: formData.username,
+          to_name: "Akarsh Bajpai",
+          from_email: formData.email,
+          to_email: "onlyakarsh@gmail.com",
+          message: formData.query,
+        },
+        'Zqbt0KdyiJ6MSrYRy'
+      )
+      .then(
+        () => {
+          alert("Thank you. I will get back to you soon...")
+        },
+        //@ts-ignore
+        (error) => {
+          alert("Ahh, something went wrong. Please try again.");
+        }
+      );
+  };
 
   return (
     <Drawer >
@@ -32,17 +71,16 @@ export function Contact() {
           <DrawerHeader>
             <DrawerTitle>Contact us here..</DrawerTitle>
             <DrawerDescription className="">
-             <h1>Schedule Free Consultation.</h1>
-             
-               <Input className="outline-none  mt-4" placeholder="Name" />
-               <Input className="outline-none  mt-4" placeholder="Email" />
+              <h1>Schedule Free Consultation.</h1>
+              <Input value={formData.username} onChange={handleChange} className="outline-none  mt-4" placeholder="Name" />
+              <Input value={formData.email} onChange={handleChange} className="outline-none  mt-4" placeholder="Email" />
 
-             <Textarea placeholder="Your Query" className="mt-4" />
+              <Textarea value={formData.username} onChange={handleChange} placeholder="Your Query" className="mt-4" />
             </DrawerDescription>
           </DrawerHeader>
           <DrawerFooter>
             <DrawerClose asChild>
-              <Button variant="default">Sudmit</Button>
+              <Button onClick={handleSubmit} variant="default">Submit</Button>
             </DrawerClose>
           </DrawerFooter>
         </div>
